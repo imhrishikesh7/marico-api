ca6ae88e0ffddd641c188bdc08f2023dd4a9c590
lopajetbro <lopa@jetbro.in>
2024-10-24 14:51:22 +0530

    up

--- src/aboutus/aboutus.controller.ts
+++ src/aboutus/aboutus.controller.ts
@@ -1,18 +1,17 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AboutusService } from './aboutus.service';
 import { AboutusMember } from './entities/aboutus_member.entity';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { History } from './entities/aboutus_history.entity';
 import { Recognition } from './entities/aboutus_recognition.entity';
+import { PageService } from 'src/page/page.service';
 
 @Controller(':region/about-us')
 export class AboutusController {
-  constructor(private readonly aboutusService: AboutusService) {}
+  constructor(
+    private readonly aboutusService: AboutusService,
+    private readonly pageService: PageService,
+  ) {}
 
   @ApiBearerAuth()
   @Get('leadership')
@@ -22,19 +21,31 @@ export class AboutusController {
     return await this.aboutusService.getMembers(region, 'leadership');
   }
 
- @ApiBearerAuth()
- @Get('board-of-directors')
- async getDirectors(
-  @Param('region') region: string,
-  ): Promise<AboutusMember[]> {
-    return await this.aboutusService.getMembers(region, 'board-of-directors');
+  @ApiBearerAuth()
+  @Get('board-of-directors')
+  async getDirectors(
+    @Param('region') region: string,
+  ): Promise<{ bod: AboutusMember[]; pdfs: any[] }> {
+    const bod = await this.aboutusService.getMembers(
+      region,
+      'board-of-directors',
+    );
+
+    const pdfs = await this.pageService.findOneByUrl(
+      'about-us/board-of-directors',
+      true,
+      true,
+    );
+
+    return {
+      bod,
+      pdfs: pdfs ? [pdfs] : [],
+    };
   }
 
   @ApiBearerAuth()
   @Get('history')
-  async getHistory(
-    @Param('region') region: string,
-  ): Promise<History[]> {
+  async getHistory(@Param('region') region: string): Promise<History[]> {
     return await this.aboutusService.getHistories(region);
   }
 
