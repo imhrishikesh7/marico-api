93e3e9daa3ca2cce143ce4ae4b47346e439d1745
lopajetbro <lopa@jetbro.in>
2024-11-28 13:12:56 +0530

    up

---
+++ migrations/1732779406091-brandedit.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Brandedit1732779406091 implements MigrationInterface {
+    name = 'Brandedit1732779406091'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`page_content\` ADD \`add_choice\` json NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`brand\` ADD \`facebook_url\` varchar(255) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`brand\` ADD \`twitter_url\` varchar(255) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`brand\` ADD \`youtube_url\` varchar(255) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE \`brand\` ADD \`web_url\` varchar(255) NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`brand\` DROP COLUMN \`web_url\``);
+        await queryRunner.query(`ALTER TABLE \`brand\` DROP COLUMN \`youtube_url\``);
+        await queryRunner.query(`ALTER TABLE \`brand\` DROP COLUMN \`twitter_url\``);
+        await queryRunner.query(`ALTER TABLE \`brand\` DROP COLUMN \`facebook_url\``);
+        await queryRunner.query(`ALTER TABLE \`page_content\` DROP COLUMN \`add_choice\``);
+    }
+
+}
