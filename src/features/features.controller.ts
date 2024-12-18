17e3f3e8a1fce7b5c8315a4454216a04ac6154b3
lopajetbro <lopa@jetbro.in>
2024-12-18 15:47:01 +0530

    up

--- src/features/features.controller.ts
+++ src/features/features.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { FeaturesService } from './features.service';
 
 @Controller('features')
