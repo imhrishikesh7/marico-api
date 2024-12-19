0c49d0c7e782c7dbf49d203cefbd798ac47d1ae6
Sharva Jethwa <sharva@jetbro.in>
2024-12-19 12:26:21 +0530

    feat(controller): add versioning and a new endpoint for health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, VERSION_NEUTRAL } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  version: VERSION_NEUTRAL,
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return 'ok';
+  }
 
   @Get('/info')
   getInfo(): object {
