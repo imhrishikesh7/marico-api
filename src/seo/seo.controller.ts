45d3ddfce892db17a728796cb09717a3e94fdda0
lopajetbro <lopa@jetbro.in>
2024-12-19 23:14:08 +0530

    up

--- src/seo/seo.controller.ts
+++ src/seo/seo.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { SeoService } from './seo.service';
 import { Contact } from './entities/contact.entity';
 import { ApiBearerAuth, ApiBody } from '@nestjs/swagger';
